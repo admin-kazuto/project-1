d8074990bc0cbd13cc37f9b310bc80d107da8ddc
namkiem29 <namkiem0000@gmail.com>
2024-11-23 22:38:15 +0700

    thêm sửa xóa danh mục

--- views/admin/list-category.php
+++ views/admin/list-category.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,30 +8,37 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 </head>
+
 <body>
     <header>
     </header>
     <h1 class="text-center">Quản lý danh mục</h1>
+    <a href="?act=addcategory"><button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addModal">
+            <i class="fas fa-plus"></i> Thêm danh mục
+        </button></a>
+    <br>
+    <br>
     <table class="table">
         <thead class="table-dark">
-        <tr>
-            <th>Mã</th>
-            <th>Danh mục</th>
-            <th>Chức Năng</th>
-        </tr>
+            <tr>
+                <th>Mã</th>
+                <th>Danh mục</th>
+                <th>Chức Năng</th>
+            </tr>
         </thead>
         <tbody>
-        <?php foreach($data as $category) : ?>
-            <tr>
-                <td><?= $category['category_id'] ?></td>
-                <td><?= $category['category_name'] ?></td>
-                <td>
-                <button type="button" class="btn btn-success">Sửa</button>
-                <a href="index.php?act=delete-category&$category_id=<?= $category['category_id']  ?>"><button type="button" class="btn btn-success">Xoa</button></a>
-                </td>
-            </tr>
-        <?php endforeach ?>
+            <?php foreach ($data as $category) : ?>
+                <tr>
+                    <td><?= $category['category_id'] ?></td>
+                    <td><?= $category['category_name'] ?></td>
+                    <td>
+                        <a href="index.php?act=editcategory&category_id=<?= $category['category_id'] ?>"><button type="button" class="btn btn-danger">Sửa</button></a>
+                        <a href="index.php?act=deletecategory&category_id=<?= $category['category_id']  ?>"><button type="button" class="btn btn-success">Xóa</button></a>
+                    </td>
+                </tr>
+            <?php endforeach ?>
         </tbody>
     </table>
 </body>
+
 </html>
